385de02021492f7be62ede6faf234cd52a84b0c3
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-04 17:11:50 +0530

    Enums

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import { useState } from "react";
 import Navbar from "@/scenes/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
 const App = () => {
-  const [selectedPage, setSelectedPage] = useState<string>("home");
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handlScroll = () => {
+      if (window.scrollY == 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else {
+        setIsTopOfPage(false);
+      }
+    };
+    window.addEventListener("scroll", handlScroll);
+    return () => window.removeEventListener("scroll", handlScroll);
+  }, []);
 
   return (
     <div className="app bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
     </div>
   );
 };
