e31f6ebcad9d1c708de23ef52443a42e237d12ca
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-04 12:48:20 +0530

    Navbar Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import Navbar from "@/scenes/navbar";
+
 const App = () => {
-  return null;
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,25 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type LinkProps = {
+  page: string;
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
+
+const Link = ({ page, selectedPage, setSelectedPage }: LinkProps) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "");
+
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-primary-500" : ""
+      } transition duration-500 hover:text-primary-300`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+export default Link;
