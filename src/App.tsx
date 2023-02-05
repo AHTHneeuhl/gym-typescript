c15beb14cee9c7997006d8dd4630f19ab2c664f3
Shubham Patrick <ahthneeuhl@gmail.com>
2023-02-05 13:28:22 +0530

    Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -23,13 +24,14 @@ const App = () => {
   }, []);
 
   return (
-    <div className="app bg-gray-20">
+    <main className="app bg-gray-20">
       <Navbar
         isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
-    </div>
+      <Home setSelectedPage={setSelectedPage} />
+    </main>
   );
 };
 
